4d37d4102971c80e322dcc442b2c5b3aba436e08
ronishstha <stharonish@gmail.com>
2024-08-22 21:16:13 +0545

    Add build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { writeFileSync } from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "css-output",
+      writeBundle(options, bundle) {
+        const cssFileName = Object.keys(bundle).find((fileName) =>
+          fileName.endsWith(".css")
+        );
+        if (cssFileName) {
+          const cssContent = bundle[cssFileName].source;
+          writeFileSync(path.join(options.dir, "index.css"), cssContent);
+          delete bundle[cssFileName];
+        }
+      },
+    },
+  ],
+  build: {
+    lib: {
+      entry: "src/main.jsx",
+      name: "swiperify",
+      fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    cssCodeSplit: false,
+  },
+});
